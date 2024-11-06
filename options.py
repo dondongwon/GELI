3bc83e26d917eb8d9e78ef8068301a15758476de
Dong Won Lee <43928520+dondongwon@users.noreply.github.com>
2024-11-05 22:49:50 -0500

    Update options.py

--- options.py
+++ options.py
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cb4e695a497314072223b62c8f0c35928c7aeece0743b71b2087e394fa732704
-size 990
+import os, sys, pdb
+
+import argparse
+
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+parser = argparse.ArgumentParser(description='Parameters for RLMF')
+
+
+
+parser.add_argument('--model_name',  choices=('llama2','llama', 'cosmo', 'blenderbot-1b','blenderbot-400m', 'dialogpt'))
+parser.add_argument('--reward', default = 'redistributed_reward')
+parser.add_argument('--exp_name', required = True)
+parser.add_argument('--generate', action='store_true')
+parser.add_argument('--peft', action='store_true')
+parser.add_argument('--ppo_off', action='store_true')
+
+parser.add_argument('--reward_class',  choices=('overall_affect'))
+parser.add_argument('--rf_model',  choices=('GELI', 'GE', 'LI', 'sentiment', 'base', 'VFRRD_funny', 'VFRRD_intelligent', 'VFRRD_humble', 'VFRRD_sync'))
+parser.add_argument('--train', action='store_true')
+parser.add_argument('--val', action='store_true')
+parser.add_argument('--batch_size', required = True )
+parser.add_argument('--no_accel', action='store_true')
